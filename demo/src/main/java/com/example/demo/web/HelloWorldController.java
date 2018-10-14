2e24d811f0e1446f118fa5931e104185be890edd
Ariel NATAF <nataf.ariel@gmail.com>
2018-10-14 21:17:51 +0200

    Test pour vérifier que noms commencent par une majuscule

--- demo/src/main/java/com/example/demo/web/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/web/HelloWorldController.java
@@ -8,8 +8,23 @@ package com.example.demo.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @RestController
 public class HelloWorldController {
+
+	@RequestMapping("/")
+	public String home() {
+		TestsBPMN tests = new TestsBPMN();
+
+		return tests.checkCaps("ressources/diag.bpmn");
+	}
 
 	@RequestMapping("/hello")
 	public String sayHello() {
